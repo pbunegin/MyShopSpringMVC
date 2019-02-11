dd423cd7bfea9a586dfb50b3fee91e34de49bb94
Petr Bunegin <bunegin.p@gmail.com>
2019-02-11 22:54:58 +0400

    +

--- src/main/java/org/shop/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/org/shop/repository/impl/ProductRepositoryImpl.java
@@ -3,64 +3,44 @@ package org.shop.repository.impl;
 import org.hibernate.SessionFactory;
 import org.shop.data.Product;
 import org.shop.repository.ProductRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 @Transactional
 public class ProductRepositoryImpl implements ProductRepository {
-    private final static Logger LOGGER = LoggerFactory.getLogger(ProductRepositoryImpl.class);
-
     @Autowired
-    private DataSource dataSource;
+    private SessionFactory sessionFactory;
 
     @Override
     public Product getProductById(Long productId) {
-        try {
-            Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from products where id=?");
-            preparedStatement.setLong(1,productId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            resultSet.getObject(Product);
-        } catch (SQLException e) {
-            LOGGER.error(e.getSQLState());
-        }
-//        return sessionFactory.getCurrentSession().get(Product.class, productId);
+        return sessionFactory.getCurrentSession().get(Product.class, productId);
     }
 
     @Override
     public List<Product> getProducts() {
-//        return sessionFactory.getCurrentSession()
-//                .createQuery("from Product").list();
+        return sessionFactory.getCurrentSession()
+                .createQuery("from Product").list();
     }
 
     @Override
     public Long createProduct(Product product) {
-//        Long id = (Long) sessionFactory.getCurrentSession().save(product);
-//        product.refreshImgUrl();
-//        return id;
+        Long id = (Long) sessionFactory.getCurrentSession().save(product);
+        product.refreshImgUrl();
+        return id;
     }
 
     @Override
     public void updateProduct(Product product) {
-//        sessionFactory.getCurrentSession().update(product);
-//        product.refreshImgUrl();
+        sessionFactory.getCurrentSession().update(product);
+        product.refreshImgUrl();
     }
 
     @Override
     public void deleteProduct(Long productId) {
-//        sessionFactory.getCurrentSession().delete(getProductById(productId));
+        sessionFactory.getCurrentSession().delete(getProductById(productId));
     }
 }
--- src/main/java/org/shop/repository/impl/RoleRepositoryImpl.java
+++ src/main/java/org/shop/repository/impl/RoleRepositoryImpl.java
@@ -3,35 +3,142 @@ package org.shop.repository.impl;
 import org.hibernate.SessionFactory;
 import org.shop.data.UserRole;
 import org.shop.repository.RoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import javax.transaction.Transactional;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 @Transactional
 public class RoleRepositoryImpl implements RoleRepository {
+    private final static Logger LOGGER = LoggerFactory.getLogger(RoleRepositoryImpl.class);
+
     @Autowired
-    private SessionFactory sessionFactory;
+    private DataSource dataSource;
 
     @Override
     public Long createRole(UserRole role) {
-        return (Long) sessionFactory.getCurrentSession().save(role);
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "insert into roles(user_role, description) values(?,?)");
+            preparedStatement.setString(1, role.getRoleName());
+            preparedStatement.setString(2, role.getDescription());
+            if (preparedStatement.executeUpdate() > 0) {
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    return generatedKeys.getLong("role_id");
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getSQLState());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error(e.getSQLState());
+            }
+        }
+        return null;
     }
 
     @Override
     public UserRole getRoleById(Long id) {
-        return sessionFactory.getCurrentSession().get(UserRole.class, id);
+        UserRole userRole = new UserRole();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "select * from roles where role_id=?");
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            userRole.setId(resultSet.getLong("role_id"));
+            userRole.setRoleName(resultSet.getString("user_role"));
+            userRole.setDescription("description");
+        } catch (SQLException e) {
+            LOGGER.error(e.getSQLState());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error(e.getSQLState());
+            }
+        }
+        return userRole;
     }
 
     @Override
     public void updateRole(UserRole role) {
-        sessionFactory.getCurrentSession().update(role);
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "insert into roles(role_id, user_role, description) values(?,?,?)");
+            preparedStatement.setLong(1, role.getId());
+            preparedStatement.setString(2, role.getRoleName());
+            preparedStatement.setString(3, role.getDescription());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error(e.getSQLState());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                LOGGER.error(e.getSQLState());
+            }
+        }
     }
 
     @Override
     public List<UserRole> getRoles() {
-        return sessionFactory.getCurrentSession().createQuery("from Role").list();
+        UserRole userRole = new UserRole();
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from roles where role_id=?");
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            userRole.setId(resultSet.getLong("role_id"));
+            userRole.setRoleName(resultSet.getString("user_role"));
+            userRole.setDescription("description");
+        } catch (SQLException e) {
+            LOGGER.error(e.getSQLState());
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return userRole;
     }
 }
